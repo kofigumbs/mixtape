4f99ef3a2f1acd361e90a7f6b1b47b9d61dd53e6
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-15 15:27:40 -0500

    Use kofi.sexy netlify function to grab video url

--- _includes/player.js
+++ _includes/player.js
@@ -1,11 +1,11 @@
-var SOUNDCLOUD_REGEX = /soundcloud\.com/;
-var TWITTER_REGEX = /twitter\.com\/.+\/status\/(\d+)/;
-var YOUTUBE_REGEX = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+const SOUNDCLOUD_REGEX = /soundcloud\.com/;
+const TWITTER_REGEX = /twitter\.com\/.+\/status\/(\d+)/;
+const YOUTUBE_REGEX = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
 
-var current = -1;
-var container = document.querySelector("[data-container]");
-var play      = document.querySelector("[data-play]")
-var tracks    = document.querySelectorAll("[data-track]");
+let current = -1;
+const container = document.querySelector("[data-container]");
+const play      = document.querySelector("[data-play]")
+const tracks    = document.querySelectorAll("[data-track]");
 
 play.addEventListener("click", start);
 
@@ -15,9 +15,9 @@ function start() {
   play.innerText = "Play Next";
   tracks.forEach(function(el) { el.classList.remove("active") });
 
-  var match;
-  var el = tracks[current % tracks.length];
-  var trackUrl = el.dataset.track;
+  let match;
+  const el = tracks[current % tracks.length];
+  const trackUrl = el.dataset.track;
   el.classList.add("active");
 
   if (trackUrl.match(SOUNDCLOUD_REGEX)) {
@@ -30,7 +30,7 @@ function start() {
 }
 
 function setupSoundcloud(url) {
-  var el = document.createElement("iframe");
+  const el = document.createElement("iframe");
   el.src = "https://w.soundcloud.com/player/?url=" + encodeURIComponent(url) + "&auto_play=true";
   el.setAttribute("height", 166);
   el.setAttribute("width", "100%");
@@ -43,50 +43,34 @@ function setupSoundcloud(url) {
 }
 
 function setupTwitter(tweetId) {
-  // ATTEMPT 1. Use official Twitter JS library,
-  // but no methods/listeners provided to observe listening state.
-  //
-  twttr.widgets.createTweet(tweetId, container).then(function() {
-    document
-      .querySelector("twitter-widget").shadowRoot
-      .querySelector("[aria-label='Play Media']").click();
+  Promise.all([
+    twttr.widgets.createTweet(tweetId, container, { align: "center" }),
+    get("https://kofi.sexy/.netlify/functions/embed-twitter-video?tweet=" + tweetId),
+  ]).then(function(arguments) {
+    const card = arguments[0].shadowRoot.querySelector(".MediaCard");
+    card.innerHTML = "";
+
+    const source = document.createElement("source");
+    source.type = "video/mp4";
+    source.src = arguments[1];
+
+    const player = document.createElement("video");
+    player.autoplay = true;
+    player.controls = true;
+    player.style.width = "100%";
+    player.style.borderRadius = "4px";
+    player.appendChild(source);
+    card.appendChild(player);
+
     return new Promise(function(resolve, reject) {
-      window.addEventListener("message", function(event) { console.log(event) }, true);
-      // video.addEventListener("ended", resolve);
-      // video.addEventListener("error", reject);
+      player.addEventListener("ended", resolve);
+      player.addEventListener("error", reject);
     });
   }).finally(start);
-
-  // ATTEMPT 2. Create iframe (like with SoundCloud),
-  // but CORS only allows me to listen to messages that are explicitly posted.
-  // Twitter doesn't post any messages that indicate play state of media.
-  //
-  // var el = document.createElement("iframe");
-  // el.src = "https://twitter.com/i/videos/tweet/" + tweetId + "?autoplay=1";
-  // el.setAttribute("scrolling", "no");
-  // el.setAttribute("frameborder", "no");
-  // el.setAttribute("allow", "autoplay");
-  // container.appendChild(el);
-
-  // ATTEMPT 3. Use CORS proxy to get tweet or video information directly,
-  // but these require oauth.
-  //
-  // var request = new XMLHttpRequest();
-  // request.addEventListener("load", function() {
-  //   container.innerText = this.responseText;
-  // });
-  // // request.open("GET", "https://cors-anywhere.herokuapp.com/https://twitter.com/i/videos/tweet/" + tweetId);
-  // // request.open("GET", "https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/videos/tweet/config/" + tweetId + ".json");
-  // request.send();
-
-  // ATTEMPT 4. Get video ID at build-time?
-  //
-  // container.innerHTML = `
-  //   <video autoplay preload="none" playsinline="" poster="https://pbs.twimg.com/ext_tw_video_thumb/1195154006584233984/pu/img/R6qTSr--6Ol_741H.jpg" src="https://twitter.com/d6d49839-7d93-4a40-b99f-d14abb451648" style="width: 100%; height: 100%; position: absolute; background-color: black; transform: rotate(0deg) scale(1.005);"></video>
 }
 
 function setupYoutube(videoId) {
-  var player = document.createElement("div");
+  const player = document.createElement("div");
   player.id = "player";
   container.appendChild(player);
 
@@ -102,4 +86,16 @@ function setupYoutube(videoId) {
   });
 
   window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+}
+
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
+    request.addEventListener("load", function() {
+      this.status === 200 ? resolve(this.responseText) : reject(this.responseText);
+    });
+    request.addEventListener("error", reject);
+    request.open("GET", url);
+    request.send();
+  });
 }
